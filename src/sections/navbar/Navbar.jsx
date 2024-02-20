557aeb840fec2cb9f1310ef78c7a03de371e848e
ArnoldRamodike <riccoarnold23@gmail.com>
2024-02-20 20:45:40 +0200

    Adding Navbar tot the Portfolio

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import Logo from '../../assets/Arnold.jpg';
+import data from'./data';
+import {IoIosColorPalette} from 'react-icons/io'
 import './navbar.css'
 
 const Navbar = () => {
   return (
     <nav id='navbar'>
-        Navbar
+       <div className="container">
+         <div className="nav__container">
+          <a href="index.html" className='nav__logo'>
+            <img src={Logo} alt="Logo" />
+          </a>
+          <ul className='nav__menu'>
+            {
+              data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+            }
+          </ul>
+          <button id='theme__icon'>
+            <IoIosColorPalette/>
+          </button>
+          </div>  
+       </div>
     </nav>
   )
 }
